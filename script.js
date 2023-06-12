887dc55275e2514cb3d21a705aad1b64cbeffefc
Ali <aea280701@gmail.com>
2023-06-12 11:47:16 +0300

    adding functionality

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-let data = []
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const twitterBtn = document.querySelector(".b1"); 
+const newQuoteBtn = document.querySelector(".b2"); 
+let data = [];
 
 //random quote 
 function getRandQuote() {
     const randQoute = data[Math.floor(Math.random() * data.length)]
-    console.log(randQoute)
+    //checking whether there is an author or not
+    if(!randQoute.author) {
+        author.textContent = "Anonymous";
+    } else {
+        author.textContent = randQoute.author;
+    }
+    //checking the quote length to change the font size
+    if(randQoute.text.length > 100) {   
+        quote.classList.add("long-quote")
+    } else {
+        quote.classList.remove("long-quote")
+    }
+    quote.textContent = randQoute.text;
+}
+//tweeting
+function tweet() {
+    url = `https://twitter.com/intent/tweet?text=${quote.textContent} - ${author.textContent}`
+    window.open(url, "_blank")
 }
 
 // getting the quote from the api
@@ -13,11 +34,13 @@ async function getQuote() {
         const res = await fetch(url)
         data = await res.json()
         getRandQuote()
-
     } catch (error) {
         //error
     }
 }
 
+
+newQuoteBtn.addEventListener("click", getRandQuote)
+twitterBtn.addEventListener("click", tweet)
 getQuote()
 
